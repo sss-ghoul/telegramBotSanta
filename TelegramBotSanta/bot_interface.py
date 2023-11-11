8c90d0a8ffc237a918b23b872854335f6f625bc3
Grigory <barbkadze.com>
2023-11-12 00:15:51 +0300

    function1

--- TelegramBotSanta/bot_interface.py
+++ TelegramBotSanta/bot_interface.py
@@ -3,14 +3,12 @@ from telebot import types
 import datetime
 import sqlite3
 
-con = sqlite3.connect("student.db")
+con = sqlite3.connect("student.db", check_same_thread=False)
 cursor = con.cursor()
-
 
 bot = telebot.TeleBot('6503197973:AAHdYlwaDw4NshV8wjhS_5QUwJ2ICL8gQWE')
 
 user = {}
-new_user = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -26,6 +24,7 @@ def start(message):
 
     bot.send_message(user_id, "Для участия в Тайном Санте, нажми 'Начать'.", reply_markup=markup)
 
+
 # Обработчик "Начать"
 @bot.message_handler(func=lambda message: message.text == "Начать")
 def register_user(message):
@@ -35,26 +34,30 @@ def register_user(message):
     bot.send_message(user_id, "1. Ваше ФИО:")
     bot.register_next_step_handler(message, get_fullname)
 
+
 def get_fullname(message):
     user_id = message.chat.id
     user[user_id]["fullname"] = message.text
 
-    bot.send_message(user_id, "2. Факультет:")
-    bot.register_next_step_handler(message, get_faculty)
-
-def get_faculty(message):
-    user_id = message.chat.id
-    user[user_id]["faculty"] = message.text
-
-    bot.send_message(user_id, "3. Группа:")
+    bot.send_message(user_id, "2. Группа:")
     bot.register_next_step_handler(message, get_group)
+
 
 def get_group(message):
     user_id = message.chat.id
     user[user_id]["group"] = message.text
 
+    bot.send_message(user_id, "3. Факультет:")
+    bot.register_next_step_handler(message, get_faculty)
+
+
+def get_faculty(message):
+    user_id = message.chat.id
+    user[user_id]["faculty"] = message.text
+
     bot.send_message(user_id, "4. Список желаний:")
     bot.register_next_step_handler(message, get_wishlist)
+
 
 def get_wishlist(message):
     user_id = message.chat.id
@@ -62,12 +65,38 @@ def get_wishlist(message):
 
     bot.send_message(user_id, "Спасибо за регистрацию!")
 
-    # можно установить дату
-    draw_date = datetime.datetime.now() + datetime.timedelta(days=14)
-    bot.send_message(user_id, f"Жеребьевка состоится {draw_date.strftime('%Y-%m-%d %H:%M')}.")
-    bot.send_message(user_id, f"Количество участников {len(user)}.")
-    print(user)
+    cursor.execute('INSERT INTO Student (Name, study_group, Faculty, Wishes) VALUES (?, ?, ?, ?)',
+                   (user[user_id]["fullname"], user[user_id]["group"], user[user_id]["faculty"],
+                    user[user_id]["wishlist"]))
+    con.commit()
+    bot.register_next_step_handler(message, result)
+
+
+def result(message):
+    print(1)
+    user_id = message.chat.id
+    bot.send_message(user_id, f'Готово')
+
+    # cursor.execute('SELECT * FROM Student')
+    # students = cursor.fetchall()
+    # for i in range(0, len(students)):
+    #     if i == len(students)-1:
+    #         cursor.execute('INSERT INTO Gift (Santa, Kid) VALUES (?, ?)',
+    #                        (students[i][1], students[0][1]))
+    #     else:
+    #         cursor.execute('INSERT INTO Gift (Santa, Kid) VALUES (?, ?)',
+    #                        (students[i][1], students[i+1][1]))
+    #
+    #
+    # cursor.execute('SELECT Santa, Kid FROM Gift WHERE Santa =?', (user[user_id]["fullname"],))
+    # student = cursor.fetchall()
+    #
+    # cursor.execute('SELECT Wishes FROM Student WHERE Name=?', (user[user_id]["fullname"]))
+    # wish = cursor.fetchall()
+    #
+    # bot.send_message(user_id, f'Привет, {student[0][0]}, вы дарите подарок {student[0][1]}. '
+    #       f'\nЕго список желаний: {wish[0][0]}')
 
 
 # Запускаем бота
-bot.polling()
+
--- TelegramBotSanta/main.py
+++ TelegramBotSanta/main.py
@@ -1,45 +1,28 @@
 import sqlite3
+import bot_interface
 
 con = sqlite3.connect('student.db')
 cursor = con.cursor()
 
-
-def result():
-    name = input("Введите имя ")
-
-    cursor.execute('SELECT Santa, Kid FROM Gift WHERE Santa =?', (name,))
-    student = cursor.fetchall()
-
-    cursor.execute('SELECT Wishes FROM Student WHERE Name =?', (name,))
-    wish = cursor.fetchall()
-
-    print(f'Привет, {student[0][0]}, вы дарите подарок {student[0][1]}. '
-          f'\nЕго список желаний: {wish[0][0]}')
-
-    # cursor.execute('SELECT * FROM Student')
-    # students = cursor.fetchall()
-    # for i in range(0, len(students)):
-    #     if i == len(students)-1:
-    #         cursor.execute('INSERT INTO Gift (Santa, Kid) VALUES (?, ?)',
-    #                     (students[i][1], students[0][1]))
-    #     else:
-    #         cursor.execute('INSERT INTO Gift (Santa, Kid) VALUES (?, ?)',
-    #                    (students[i][1], students[i+1][1]))
+# def result():
+#     print(1)
+#
+#     cursor.execute('SELECT Santa, Kid FROM Gift WHERE Santa =?', (bot_interface.user[user_id]["fullname"],))
+#     student = cursor.fetchall()
+#
+#     cursor.execute('SELECT Wishes FROM Student WHERE Name =?', (name,))
+#     wish = cursor.fetchall()
 
 
-str = input("Зарегистрироваться? ")
-if str == 'Да':
-    Name = input('Введите имя: ')
-    study_group = input('Введите группу: ')
-    Faculty = input('Введите факультет: ')
-    Wishes = input('Введите список желаний: ')
+# cursor.execute('DELETE FROM Student WHERE id = ?', (1,))
 
-    cursor.execute('INSERT INTO Student (Name, study_group, Faculty, Wishes) VALUES (?, ?, ?, ?)',
-                   (Name, study_group, Faculty, Wishes))
 
-x = input("Вывести результат? ")
-if x == 'да' or x == 'Да':
-    result()
+s = input("Вывести результат? Да/Нет\n")
+
+bot_interface.bot.polling()
+
+if s == 'lf' or s == 'LF' or s == 'Lf' or s == 'да' or s == 'ДА' or s == 'Да':
+    bot_interface.result()
 
 con.commit()
 con.close()
